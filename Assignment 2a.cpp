d37cc916bd79a0680b6acde17c9cfcfe2bcb83f9
MadEldar <49936697+MadEldar@users.noreply.github.com>
2019-05-03 13:43:20 +0700

    BTVN 24/4/2019

---
+++ Assignment 2a.cpp
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//int main(){
+//	int a=0;
+//	printf("Nhap vao 1 so: ");
+//	scanf("%d",&a);
+//	switch(a){
+//		case 2: printf("\nHom nay la thu 2");
+//		break;
+//		case 3: printf("\nHom nay la thu 3");
+//		break;
+//		case 4: printf("\nHom nay la thu 4");
+//		break;
+//		case 5: printf("\nHom nay la thu 5");
+//		break;
+//		case 6: printf("\nHom nay la thu 6");
+//		break;
+//		case 7: printf("\nHom nay la thu 7");
+//		break;
+//		case 8: printf("\nHom nay la chu nhat");
+//		break;
+//		default: printf("\nDay khong phai 1 ngay trong tuan");
+//		break;
+//	}
+//	return 0;
+//}
