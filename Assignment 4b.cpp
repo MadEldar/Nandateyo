33b80fe319a6287e60179fb5b4176ee373ea959a
MadEldar <49936697+MadEldar@users.noreply.github.com>
2019-05-06 15:30:34 +0700

    Update Assignment 4b.cpp

--- Assignment 4b.cpp
+++ Assignment 4b.cpp
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(void){
-	long long list[100],i=2;
-	list[0]=1;
-	list[1]=2;
+	double list[100];
+	int i=2;
+	list[0]=0.00;
+	list[1]=1.00;
 	for(i=2;i<100;i++){
 		list[i]=list[i-1]+list[i-2];
+		printf("So thu 100 co gia tri %.0lf\n",list[i-1]);
 	}
-	printf("So thu 100 co gia tri %lld",list[i-1]);
 	return 0;
 }
